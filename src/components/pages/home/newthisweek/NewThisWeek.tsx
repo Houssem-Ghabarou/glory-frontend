9ba789c51981f01bf0633fbcff106d3eb20f6a7b
Houssem Ghabarou <44008549+Houssem-Ghabarou@users.noreply.github.com>
2025-05-11 17:50:22 +0100

    new this week design

--- src/components/pages/home/newthisweek/NewThisWeek.tsx
+++ src/components/pages/home/newthisweek/NewThisWeek.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { mockPropsCard } from "@/mock/items";
 import DefaultImageSlider from "@/components/shared/sliders/defaultImageSlider";
+import { titleClass } from "@/lib/tailwind/classNames";
 const NewThisWeek = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-8 w-full items-stretch">
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className={titleClass}>NEW</h1>
+          <h1 className={titleClass}>THIS WEEK</h1>
+        </div>
+        {/* see all  */}
+        <div className="hidden lg:flex justify-between items-center self-end ">
+          <div className="w-full">
+            <button className="text-secondary  font-[400] text-[16px]">
+              See All
+            </button>
+          </div>
+        </div>
+      </div>
+
       <DefaultImageSlider data={mockPropsCard} />
     </div>
   );
